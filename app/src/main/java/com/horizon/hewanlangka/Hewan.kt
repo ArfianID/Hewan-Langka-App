964136b91d38381ceba8c9f7757c2803587a1efd
Arfian <arfian230@gmail.com>
2022-08-14 08:26:27 +0700

    Revert "Add existing project files to Git"
    This reverts commit f5765a78356fb5f3d8aaf4fe03b9e7f32068630d.

--- app/src/main/java/com/horizon/hewanlangka/Hewan.kt
+++
@@ -1,9 +0,0 @@
-package com.horizon.hewanlangka
-
-data class Hewan(
-    var name: String = "",
-    var asal: String = "",
-    var photo: Int = 0,
-    var photo1: Int = 0,
-    var detail: String = ""
-)
